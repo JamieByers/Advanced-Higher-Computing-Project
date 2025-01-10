a96dcfe28f4b27338f2b98acfe5434db69bc36f4
Jamie Byers <102622047+JamieByers@users.noreply.github.com>
2025-01-10 13:04:05 +0000

    changed sort to work with any key

--- sorter.py
+++ sorter.py
@@ -2,12 +2,13 @@ class Sorter:
     def __init__(self, ):
         self.products = []
 
-    def sort(self):
+    def sort(self, key):
         n = len(self.products)
         swapped = True
         while swapped and n >= 0:
             swapped = False 
             for i in range(n-1):
-                if self.products[i].int_price > self.products[i+1].int_price:
+                # Search by the key inputted using getattr: this is instead of self.products[i].key. This means I dont have to write multiple search algorithms 
+                if getattr(self.products[i], key) > getattr(self.products[i + 1], key):
                     self.products[i], self.products[i+1] = self.products[i+1], self.products[i]
             n -= 1
