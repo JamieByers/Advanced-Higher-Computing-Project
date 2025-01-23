9db91bf31e77087f316916636b4837a34b5f327c
Jamie Byers <102622047+JamieByers@users.noreply.github.com>
2025-01-23 11:04:54 +0000

    finished search

--- searcher.py
+++ searcher.py
@@ -1,11 +1,19 @@
 class Searcher:
     def __init__(self, products):
         self.products = products
-        n = len(products)
 
+
+    def search(self, target, value):
         left = 0 
-        right = len(products)
+        right = len(self.products)
         found = False
 
         while left <= right and not found:
-            pass
+            middle = (left + right) // 2
+
+            if getattr(value, self.products[middle]) == target: 
+                return middle
+            elif getattr(value, self.products[middle]) < target: 
+                left = middle + 1
+            else:
+                right = middle - 1 
